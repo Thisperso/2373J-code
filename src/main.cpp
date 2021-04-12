3293fe518f36b956f42c4dabc8a95b504dd060f0
Jonathon Foltyn <100000303@ccisd.net>
2021-04-12 10:51:41 -0500

    rip skills, auton jumper switch selector work

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ double ptunedrive;
 int PID (double dist, double curr, double p){
   double mtrspeed = p *  (dist - curr);
 
-  double velmax = 90;
+  double velmax = 80;
 
   if (mtrspeed > velmax){
     mtrspeed = velmax;
@@ -61,7 +61,7 @@ int Turn_PID (double degrees, double curr_t, double t){
 
   double mtrspeed = t * error;
 
-  double velmax = 70;
+  double velmax = 60;
 
   if (mtrspeed > velmax){
     mtrspeed = velmax;
@@ -126,7 +126,7 @@ int drive(double dist, double hold_angle){
 
 int turn(int turn_d){
   double curr_t = inert.rotation(degrees);
-  double turn_tune = 4;
+  double turn_tune = 2;
   double variance;
   Brain.Screen.clearScreen();
   Brain.Screen.print("turn to: ");
@@ -201,11 +201,14 @@ void display_position(){
   //controller display
   Controller1.Screen.clearScreen();
   Controller1.Screen.setCursor(1,1);
-  Controller1.Screen.print("x: ", robot_pos.GetX());
+  Controller1.Screen.print("x: ");
+  Controller1.Screen.print(robot_pos.GetX());
   Controller1.Screen.setCursor(2,1);
-  Controller1.Screen.print("y: ", robot_pos.GetY());
+  Controller1.Screen.print("y: ");
+  Controller1.Screen.print(robot_pos.GetY());
   Controller1.Screen.setCursor(3,1);
-  Controller1.Screen.print("heading: ", inert.rotation());
+  Controller1.Screen.print("heading: ");
+  Controller1.Screen.print(inert.rotation());
 
   //Brain display
   Brain.Screen.clearScreen();
@@ -223,7 +226,7 @@ void display_position(){
 
 
 //DRIVE_TO FUNCTION
-void driveTo(double x, double y, double tolerance){
+void driveTo(double x, double y, double tolerance, bool backwards){
   
   /*-------------------- LOGIC -------------------------*/
   /* inititalize variables and reset encoders           */
@@ -303,15 +306,36 @@ void driveTo(double x, double y, double tolerance){
     //calcualting the turn error and speeds
     double turn_error = Turn_PID(angl, inert.rotation(degrees), .75);
 
+    if (backwards){
+      angl = angl + 180;
+
+      double speed = PID(dist, 0, 90); //+ turn_error;
+      //double rspeed = PID(dist, 0, 90) - turn_error;
+
+      if ((speed - lfdrive.velocity(pct)) > 50){
+      speed = 50;
+      }
+
+      //motor drive functions
+      lft.spin(reverse, speed - turn_error, pct);
+      rht.spin(reverse, speed + turn_error, pct);
+
+    }
+
     //displaying the position on the field
+    else {
+      double speed = PID(dist, 0, 90); //+ turn_error;
+      //double rspeed = PID(dist, 0, 90) - turn_error;
 
-    double lspeed = PID(dist, 0, 35) + turn_error;
-    double rspeed = PID(dist, 0, 35) - turn_error;
+      if ((speed - lfdrive.velocity(pct)) > 50){
+        speed = 50;
+      }
 
-    //motor drive functions
-    lft.spin(forward, lspeed, pct);
-    rht.spin(forward, rspeed, pct);
-    //display_position();
+      //motor drive functions
+      lft.spin(forward, speed + turn_error, pct);
+      rht.spin(forward, speed - turn_error, pct);
+    }
+    display_position();
 
     Brain.Screen.clearScreen();
     Brain.Screen.setCursor(1,1);
@@ -333,10 +357,10 @@ void driveTo(double x, double y, double tolerance){
 
     Brain.Screen.setCursor(5,1);
     Brain.Screen.print("lft: ");
-    Brain.Screen.print(lspeed);
+    Brain.Screen.print(PID(dist, 0, 90) + turn_error);
     Brain.Screen.setCursor(6,1);
     Brain.Screen.print("rht: ");
-    Brain.Screen.print(rspeed);
+    Brain.Screen.print(PID(dist, 0, 90) - turn_error);
     Brain.Screen.setCursor(7,1);
     Brain.Screen.print("dist: ");
     Brain.Screen.print(fabs(dist));
@@ -354,12 +378,9 @@ void driveTo(double x, double y, double tolerance){
     Brain.Screen.print("not done");
     wait(10, msec);
   }
-
-
-
-//Brain.Screen.clearScreen();
+    //Brain.Screen.clearScreen();
     Brain.Screen.setCursor(11,1);
-    Brain.Screen.print("done");
+    Brain.Screen.print("NAE NAE");
 
 }
 
@@ -458,16 +479,28 @@ void autonomous(void) {
 
   //------------------------ ISM TESTING -------------------------//
 
-    driveTo(3, 0, 3);
-    driveTo(3, -4, 3);
-    driveTo(3, 0, 3);
-    driveTo(3, 3, 3);
-    driveTo(-4, 3, 3);
-    driveTo(0, 1, .5);
-    driveTo(1, 3, 2);
-    driveTo(3, 0, .5);
+    driveTo(2, 0, 1, false);
+    driveTo(3.5, 3.5, .5, false);
+    score.spin(fwd, 100, pct);
+    wait(1.25, sec);
+    score.stop(hold);
+    driveTo(3.5, 3.5, 2, true);
+    intakes.spin(fwd, 100, pct);
+    elevator.spin(fwd, 100, pct);
+    driveTo(3, 0, .5, false);
+    driveTo(3, 0, 2, true);
+    driveTo(-2, -2, .5, false);
+    score.spin(fwd, 100, pct);
+    wait(2, sec);
+    score.stop(hold);
+    driveTo(1, 1, .5, true);
     lft.stop(hold);
     rht.stop(hold);
+    /*driveTo(0, 1, .5);
+    driveTo(1, 3, 2);
+    driveTo(3, 0, .5);*/
+    //lft.stop(hold);
+    //rht.stop(hold); 
 
     /*driveTo(6, 6, 2);
     driveTo(4, 4, .5);*/
@@ -476,7 +509,7 @@ void autonomous(void) {
 
 
   //corner and mid-mid auton
-  /*if (auton1.pressing()){
+  if (auton1.pressing()){
     drive(24, 225);
     turn(225);
     intakes.spin(reverse, 100, pct);
@@ -534,133 +567,132 @@ void autonomous(void) {
     wait(2, sec);
     drive(-28, 180);
   }
-*/
   //skills auton
   //else{
     
   //first tower
-  /*intakes.spin(reverse, 100, pct);
-  drive(28, 0);
+  /*drive(28, 0);
   intakes.stop(hold);
-  turn(135);
-  drive(34, 135);
+  turn(130);
+  drive(35, 130);
   score.spin(fwd, 100, pct);
-  wait(2,sec);
+  wait(1,sec);
   score.stop(hold);
 
   //second tower
-  drive(-18, 135);
+  drive(-18, 130);
   turn(0);
   intakes.spin(fwd, 100, pct);
-  elevator.spin(fwd, 100, pct);
-  drive(48, 5);
+  drive(50, 7);
   turn(90);
-  intakes.spin(reverse,100,pct);
-  drive(14, 90);
+  intakes.stop(hold);
+  drive(8, 90);
   score.spin(fwd, 100, pct);
   wait(2,sec);
   score.stop(hold);
 
   //third tower
-  drive(-18, 90);
+  drive(-12, 90);
   turn(0);
-  drive(36, 0);
+  drive(38, 0);
   turn(90);
   intakes.spin(fwd, 100, pct);
-  drive(23, 90);
-  drive(-23, 90);
+  elevator.spin(fwd, 100, pct);
+  drive(18, 90);
+  drive(-16, 90);
   turn(0);
   drive(8, 0);
   turn(45);
-  drive(24, 45);
+  drive(21, 45);
+  intakes.stop(hold);
   score.spin(fwd, 100, pct);
-  wait(3,sec);
+  wait(1,sec);
   score.stop(hold);
 
   //fourth tower
-  drive(-54, 45);
+  drive(-51, 45);
   turn(273);
   intakes.spin(fwd, 100, pct);
   elevator.spin(fwd, 100, pct);
   drive(24, 273);
   turn(0);
   intakes.spin(reverse, 100, pct);
-  drive(44, 0);
+  drive(42, 0);
   score.spin(fwd, 100, pct);
-  wait(2,sec);
+  wait(1,sec);
   score.stop(hold);
 
   //fifth tower
-  drive(-20, 0);
+  drive(-6, 0);
   intakes.spin(fwd,100,pct);
   turn(273);
-  drive(50, 273);
+  drive(52, 273);
   turn(-45);
-  drive(24, -45);
+  drive(15, -45);
   intakes.spin(reverse, 100, pct);
   score.spin(fwd, 100, pct);
-  wait(2,sec);
+  wait(1,sec);
   score.stop(hold);
 
   //sixth tower
-  drive(-24, -45);
+  drive(-18, -45);
   turn(183);
   intakes.spin(fwd,100,pct);
   elevator.spin(fwd, 100, pct);
-  drive(50, 183);
+  drive(48, 183);
   turn(270);
   intakes.spin(reverse, 100, pct);
-  drive(14, 270);
+  drive(8, 270);
   score.spin(fwd, 100, pct);
-  wait(2,sec);
+  wait(1,sec);
   score.stop(hold);
 
   //seventh tower
-  drive(-18, 270);
+  drive(-10, 270);
   intakes.spin(fwd,100,pct);
   elevator.spin(fwd, 100, pct);
   turn(90);
   drive(24, 90);
   turn(180);
   drive(24, 180);
-  turn(225);
+  turn(223);
   intakes.spin(reverse, 100, pct);
-  drive(60, 225);
+  drive(51, 223);
   score.spin(fwd, 100, pct);
-  wait(2,sec);
+  wait(1,sec);
   score.stop(hold);
 
   //eighth tower 
-  drive(-34, 225);
+  drive(-32, 220);
   turn(270);
   intakes.spin(fwd, 100, pct);
   elevator.spin(fwd, 100, pct);
-  drive(23, 225);
-  drive(-23, 225);
-  turn(135);
-  drive(13, 135);
-  turn(90);
-  drive(26, 90);
+  drive(23, 270);
+  drive(-66, 285);
   turn(180);
-  drive(16, 180);
+  drive(10, 180);
+  intakes.spin(reverse, 100, pct);
   score.spin(fwd, 100, pct);
-  wait(2,sec);
+  wait(1,sec);
   score.stop(hold);
 
   //ninth tower
-  drive(-18, 180);
+  drive(-10, 180);
   turn(0);
   intakes.spin(fwd, 100, pct);
   elevator.spin(fwd, 100, pct);
   drive(24, 0);
-  drive(24, 10);
-  drive(-12, 10);
+  drive(12, -20);
+  drive(15, 0);
+  drive(-12, 0);
+  drive(15, 0);
+  drive(-12, -20);
   drive(8, 0);
   score.spin(fwd, 100, pct);
-  wait(2,sec);
+  wait(1,sec);
   score.stop(hold);
-  drive(-8, 0);*/
-
+  drive(-8, 0);
+  */
   //}
   //skills autononomous
   /*turn(180);
@@ -711,6 +743,7 @@ void autonomous(void) {
 
 void usercontrol(void) {
   // User control code here, inside the loop
+  bool drive_toggle = false;
   while (1) {
 
 
